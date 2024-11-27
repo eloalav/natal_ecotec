01a1ad5fe73f0c36945dd28c194e0c148582541d
eloa <eloalav@gmail.com>
2024-11-27 14:35:23 -0300

    Natal

--- index.js
+++ index.js
@@ -1,57 +1,106 @@
-// Função chamada quando o jogador errar
-function errou(cor) {
-    currentAttempt += 1;
+// Variáveis "globais para controle"
+let client = null;
+let connected = false;
+let connectionAttempt = 1;
 
-    // Adiciona o presente errado na caixinha
-    adicionarPresenteNaCaixinha(cor);
+// Cria identificar único do cliente conectado
+const clientId = "natalecotec_" + Math.floor(Math.random() * 900) + 100;
 
-    // Verifica se o jogador ainda tem tentativas
-    if (currentAttempt >= maxAttempts) {
-        alert("Você perdeu! O jogo terminou!");
-        // Lógica para esconder o presente (por exemplo, removendo-o da tela)
-        esconderPresente();
-    } else {
-        alert(`Você errou! Tente novamente! Tentativas restantes: ${maxAttempts - currentAttempt}`);
-        
-        // Exibe o presente, mesmo que o jogador tenha errado
-        mostrarPresente();
-        
-        // Atualiza o número de tentativas restantes na interface
-        atualizarTentativas();
+// Configurações do broker, usa TLS
+const hostname = "broker.hivemq.com";
+const port = 8884;
+const path = "/mqtt";
+const user = "MY_USER";
+const pass = "MY_PASSWORD";
+
+// Configurações do cliente
+const keepAlive = 60;
+const timeout = 30;
+const tls = true;
+const cleanSession = false;
+const lastWillTopic = `natalecotec/${clientId}`;
+const lastWillQos = 0;
+const lastWillRetain = true;
+const lastWillMessageVal = `Last will of ${clientId}`;
+
+// Cria objeto
+client = new Paho.MQTT.Client(hostname, Number(port), path, clientId);
+
+// Configuração de Callbacks
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = onMessageArrived;
+client.onConnected = onConnected;
+
+let lastWillMessage = new Paho.MQTT.Message(lastWillMessageVal);
+lastWillMessage.destinationName = lastWillTopic;
+lastWillMessage.qos = lastWillQos;
+lastWillMessage.retained = lastWillRetain;
+
+const connectOptions = {
+    invocationContext: {
+        host: hostname,
+        port: port,
+        path: client.path,
+        clientId: clientId
+    },
+    timeout: timeout,
+    keepAliveInterval: keepAlive,
+    cleanSession: cleanSession,
+    useSSL: tls,
+    onSuccess: onConnect,
+    onFailure: onFail,
+    userName: user,
+    password: pass,
+    willMessage: lastWillMessage
+};
+
+// Realiza conexão do cliente
+client.connect(connectOptions);
+
+
+function onConnect() { }
+
+function onConnected(reconnect, uri) {
+    connected = true;
+}
+function onFail(context) {
+    connected = false;
+}
+
+function onConnectionLost(responseObject) {
+    if (responseObject.errorCode !== 0) { }
+
+    // Depois de 2 segundos tenta reconexão
+    const reconnectSeconds = 2 * connectionAttempt
+    connectionAttempt += 1
+    setTimeout(() => {
+        if (connectOptions.hasOwnProperty("mqttVersionExplicit")) {
+            delete connectOptions.mqttVersionExplicit
+        }
+        client.connect(connectOptions)
+    }, reconnectSeconds * 1000);
+}
+function onMessageArrived(message) { }
+
+
+function acertou(cor) {
+    const message = new Paho.MQTT.Message("Ligar");
+    message.qos = 0;
+
+    if (cor === "blue"){
+        message.destinationName = `natalecotec/ligar/faixa/blue`;
+    } else if (cor === "red"){
+        message.destinationName = `natalecotec/ligar/faixa/red`;
+    } else if (cor === "green"){
+        message.destinationName = `natalecotec/ligar/faixa/green`;
+    } else if ( cor === "yellow"){
+        message.destinationName = `natalecotec/ligar/faixa/yellow`;
+    } else if ( cor === "estrela"){
+        message.destinationName = `natalecotec/ligar/faixa/estrela`;
+    } else if ( cor === "apagar"){
+        message.destinationName = `natalecotec/ligar/faixa/apagar`;
     }
-}
-
-// Função para exibir o presente (se não desapareceu)
-function mostrarPresente() {
-    // Asegura que o presente seja visível
-    const presente = document.getElementById("presente");
-    if (presente) {
-        presente.style.visibility = "visible"; // Torna o presente visível
-    }
-}
-
-// Função para esconder o presente (caso o jogo termine)
-function esconderPresente() {
-    const presente = document.getElementById("presente");
-    if (presente) {
-        presente.style.visibility = "hidden"; // Torna o presente invisível ao término
-    }
-}
-
-// Função para adicionar o presente na caixinha
-function adicionarPresenteNaCaixinha(cor) {
-    const caixinha = document.getElementById("caixinha");
-    const presenteErrado = document.createElement("div");
-    presenteErrado.classList.add("presente");
-    presenteErrado.textContent = `Presente de cor ${cor} errado!`;
-
-    // Aqui você pode customizar o estilo, como adicionar uma imagem ou cor ao "presente"
-    presenteErrado.style.backgroundColor = cor; // Pode usar uma cor, imagem, etc.
-
-    caixinha.appendChild(presenteErrado);
-}
-
-// Função para atualizar o número de tentativas restantes
-function atualizarTentativas() {
-    document.getElementById("tentativas").textContent = `Tentativas restantes: ${maxAttempts - currentAttempt}`;
+    
+    // Publica a mensagem
+    client.send(message)
 }
